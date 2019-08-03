5d5bd36119ab242fcaff14115d030486ea9a6cf8
we dul <wedul.chul@gmail.com>
2019-08-03 10:59:08 +0900

    https://github.com/weggdul/algorithm/issues/1 [1주차][수학][정철][자바] stack을 이용한 막대기 문제 풀기

---
+++ src/수학/막대기/Wedul.java
@@ -0,0 +1,47 @@
+package 수학.막대기;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * algorithm
+ *
+ * @author wedul
+ * @since 2019-08-03
+ **/
+public class Wedul {
+
+    private static final int INITIAL_STICK_LENGTH = 64;
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(calculateMakeStickCnt(scan.nextInt()));
+    }
+
+    private static int calculateMakeStickCnt(int inputStickLength) {
+        Stack<Integer> stickLengths = new Stack<>();
+        stickLengths.push(INITIAL_STICK_LENGTH);
+
+        while (!isEqualInputStickLength(stickLengths, inputStickLength)) {
+            int minStickLength = stickLengths.pop() / 2;
+
+            stickLengths.push(minStickLength);
+            if (getStickLength(stickLengths) < inputStickLength) {
+                stickLengths.push(minStickLength);
+            }
+        }
+
+        return stickLengths.size();
+    }
+
+    private static boolean isEqualInputStickLength(Stack<Integer> stickLengths, int inputStickLength) {
+        return getStickLength(stickLengths) == inputStickLength;
+    }
+
+    private static int getStickLength(Stack<Integer> stickLengths) {
+        return stickLengths.stream().reduce(Integer::sum).get();
+    }
+
+}
